a929ab246f62a7b69ee71880fb9121a2dec64a45
214g5a0510 <112165415+214g5a0510@users.noreply.github.com>
2022-09-16 15:55:48 +0530

    created sample.c

---
+++ sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate:B.OBIREDDY
+   Roll No: 214G5A0510
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 29-08-2022
+*/
