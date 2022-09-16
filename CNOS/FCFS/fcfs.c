037a9ea47a77606f421a4f74f25922029423906b
214g5a0510 <112165415+214g5a0510@users.noreply.github.com>
2022-09-16 15:32:50 +0530

    Create fcfs.c

---
+++ CNOS/FCFS/fcfs.c
@@ -0,0 +1,6 @@
+Name Of the Candidate: B.Obi Reddy
+   Roll No: 214g5a0510
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
